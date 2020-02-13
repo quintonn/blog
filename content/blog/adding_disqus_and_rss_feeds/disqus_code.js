d83d1e12c551811e29ed3af2afabf947c9df3b92
Quintonn <q10athome@gmail.com>
2020-02-13 18:44:13 +0200

    added a post on how to add comments and subscriptions to my blog

---
+++ content/blog/adding_disqus_and_rss_feeds/disqus_code.js
@@ -0,0 +1,30 @@
+﻿let disqusSite = '<MY DISQUS SITE NAME>';
+let w = window;
+
+let disqusConfig = function ()
+{
+    this.page.identifier = category + "_" + name;
+    this.page.url = window.location.href;
+};
+
+if (!w.DISQUS)
+{
+    console.log('calling embed, not reset, for disqus');
+
+    w.disqus_config = disqusConfig;
+
+    let d = document, s = d.createElement('script');
+
+    s.src = 'https://' + disqusSite + '.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', new Date() + "");
+
+    (d.head || d.body).appendChild(s);
+}
+else
+{
+    console.log('calling disqus.reset');
+    w.DISQUS.reset({
+        reload: true,
+        config: disqusConfig
+    });
+}
